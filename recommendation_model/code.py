37acab7b930b6a9ca409c31cb333bd422b7cf260
RedtRocks <aarav.dudeja3011@gmail.com>
2025-04-06 22:32:18 +0530

    changes

--- recommendation_model/code.py
+++ recommendation_model/code.py
@@ -1,12 +1,13 @@
 # Apriori
-
+import os
+print(os.getcwd())  # This will show you where Python is looking for the file
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Data Preprocessing
-dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+dataset = pd.read_csv("Market_Basket_Optimisation.csv", header = None)
 transactions = []
 for i in range(0, 7501):
   transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
@@ -21,7 +22,7 @@ rules = apriori(transactions = transactions, min_support = 0.003, min_confidence
 results = list(rules)
 results
 
-## Putting the results well organised into a Pandas DataFrame
+  ## Putting the results well organised into a Pandas DataFrame
 def inspect(results):
     lhs         = [tuple(result[2][0][0])[0] for result in results]
     rhs         = [tuple(result[2][0][1])[0] for result in results]
